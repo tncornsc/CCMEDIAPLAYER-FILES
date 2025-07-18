2ea68e5e8f9b23a2d8da24611631bf6147be94e0
tncornsc <chgoss1313@gmail.com>
2025-07-17 20:41:39 -0500

    Fixed a typo

--- musicdownloader.lua
+++ musicdownloader.lua
@@ -1,6 +1,6 @@
 write("enter song name")
 local name = read()
-local github = "https://raw.githubusercontent.com/tncornsx/CCMEDIAPLAYER-FILES/refs/head/main/files/"
+local github = "https://raw.githubusercontent.com/tncornsc/CCMEDIAPLAYER-FILES/refs/heads/main/files/"
 local url = github .. name
 local save = "/computer/music/" .. name .. ".dfpwm"
 shell.run("wget", url, save)
