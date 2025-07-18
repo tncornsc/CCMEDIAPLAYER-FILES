6e2dc83cca9f2bdbd56e2323d84e39f382719908
tncornsc <chgoss1313@gmail.com>
2025-07-17 20:16:57 -0500

    Create installer.lua

---
+++ installer.lua
@@ -0,0 +1,8 @@
+local github = "https://raw.githubusercontent.com/tncornsc/CCMEDIAPLAYER-FILES/refs/head/main"
+shell.run("cd ../")
+shell.run("mkdir /computer/")
+shell.run("cd /computer/")
+shell.run("mkdir /computer/music/")
+shell.run("wget", github .. "/audio.lua", "/computer/audio.lua")
+shell.run("wget", github .. "/player.lua", "/computer/player.lua")
+shell.run("wget", github .. "/musicdownloader.lua", "/computer/downloader.lua")
